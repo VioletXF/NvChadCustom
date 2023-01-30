6ae3ce591f322806056f5c3cb5209151eb188b19
VioletXF <zudevxf@gmail.com>
2023-01-30 17:47:16 +0900

    Move trouble to top

--- plugins.lua
+++ plugins.lua
@@ -17,6 +17,7 @@ return {
     requires = "nvim-tree/nvim-web-devicons",
     config = function()
       require("trouble").setup {
+        position = "top",
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
